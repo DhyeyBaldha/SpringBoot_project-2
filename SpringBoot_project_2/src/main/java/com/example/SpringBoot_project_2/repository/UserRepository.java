8ba3db5bf468a6ee2c2dcd7a8de76f36821deaa6
Dhyey <dhyeybaldha90@gmail.com>
2024-02-17 10:05:54 +0530

    springBoot add user

--- SpringBoot_project_2/src/main/java/com/example/SpringBoot_project_2/repository/UserRepository.java
+++ SpringBoot_project_2/src/main/java/com/example/SpringBoot_project_2/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.example.SpringBoot_project_2.repository;
 
 import com.example.SpringBoot_project_2.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<Users,Integer> {
 
 }
